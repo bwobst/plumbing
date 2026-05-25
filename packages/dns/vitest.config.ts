b357c61e22449f83d28f4d69487f66b0a2c4fd6d
Brian Wobst <bwobst@gmail.com>
2026-05-25 16:49:16 -0700

    Remove run script

--- packages/dns/vitest.config.ts
+++ packages/dns/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**/*.ts'],
-      exclude: ['src/**/*.test.ts', 'src/**/run.ts'],
+      exclude: ['src/**/*.test.ts'],
       reportsDirectory: './coverage',
       reporter: ['text', 'html', 'lcov'],
     },
